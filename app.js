49c01f23adf5afc0338408826ec5a2f5b4731a83
lakshmi236 <Veeksha236>
2015-07-16 12:36:40 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+﻿var app = angular.module("Main_app", []);
+
+app.controller("MainController", ["$scope", function MainController($scope) {    
+    $scope.Adress = "American University,4400 Massachusetts Ave NW,Washington, DC 20016";
+    $scope.Area = { Name: "Melbourne", Latitude: -37.8140000, Longitude: 144.9633200 };
+}]);
